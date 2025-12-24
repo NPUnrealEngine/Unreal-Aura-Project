64adc0c9fee6563b6850a11767aa6eaa62dd774d
tomneo2004 <tomneo2004@gmail.com>
2025-12-24 10:44:54 +0800

    2025/12/24
    - Fix issue for projectile hitting the character who is the instigator

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -18,20 +18,24 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 
 void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation)
 {
+	// We only spawn projectile on server
 	const bool IsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!IsServer) return;
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
 	if (CombatInterface)
 	{
+		// Get weapon socket location & rotation
 		const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
 		FRotator ProjectileRotation = (ProjectileTargetLocation - SocketLocation).Rotation();
 		ProjectileRotation.Pitch = 0.f;
 
+		// Set projectile location & rotation
 		FTransform SpawnTransform;
 		SpawnTransform.SetLocation(SocketLocation);
 		SpawnTransform.SetRotation(ProjectileRotation.Quaternion());
-		
+
+		// Spawn projectile
 		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
 			ProjectileClass,
 			SpawnTransform,
--- Source/Aura/Private/Actors/AuraProjectile.cpp
+++ Source/Aura/Private/Actors/AuraProjectile.cpp
@@ -81,6 +81,9 @@ void AAuraProjectile::Destroyed()
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
                                       UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// Ignore actor if actor is the instigator
+	if (GetInstigator() == OtherActor) return;
+	
 	UGameplayStatics::PlaySoundAtLocation(
 		this,
 		ImpactSound,
