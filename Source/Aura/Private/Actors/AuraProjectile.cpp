2f713cf405db2352016dbfe66ce804eedf172d8b
tomneo2004 <tomneo2004@gmail.com>
2026-02-05 13:10:32 +0800

    2026/2/5
    - Enemy slinghsot attack spawn rock projectile

--- Source/Aura/Private/Actors/AuraProjectile.cpp
+++ Source/Aura/Private/Actors/AuraProjectile.cpp
@@ -6,6 +6,7 @@
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
 #include "NiagaraFunctionLibrary.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
 #include "Aura/Aura.h"
 #include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
@@ -83,7 +84,10 @@ void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComp, AActo
 {
 	// Ignore actor if actor is the instigator
 	if (GetInstigator() == OtherActor) return;
-	
+	if (!UAuraAbilitySystemLibrary::IsNotFriend(GetInstigator(), OtherActor))
+	{
+		return;
+	}
 	if (!bHit)
 	{
 		UGameplayStatics::PlaySoundAtLocation(
