81c2307b4232963cf43e6fab16483737d3942e5e
tomneo2004 <tomneo2004@gmail.com>
2026-04-15 16:01:21 +0800

    2026/4/15
    - Add XP to player state

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -7,6 +7,8 @@
 #include "GameFramework/PlayerState.h"
 #include "AuraPlayerState.generated.h"
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32)
+
 /**
  * 
  */
@@ -17,15 +19,25 @@ class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInte
 
 public:
 	AAuraPlayerState();
+	
+public:
+	FOnPlayerStatChanged OnXPChangedDelegate;
+	FOnPlayerStatChanged OnLevelChangedDelegate;
 
 public:
 #pragma region Getter
 	FORCEINLINE class UAttributeSet* GetAttributeSet() const {return AttributeSet;}
-	FORCEINLINE int32 GetPlayerLevel() const { return Level; };
+	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+	FORCEINLINE int32 GetXP() const {return XP; }
 #pragma endregion  
+	
 public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	void SetXP(int32 InXP);
+	void AddToXP(int32 InXP);
+	void SetLevel(int32 InLevel);
+	void AddToLevel(int32 InLevel);
 	
 protected:
 	UPROPERTY(VisibleAnywhere)
@@ -37,8 +49,14 @@ protected:
 private:
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_Level)
 	int32 Level = 1;
+	
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_XP)
+	int32 XP = 0;
 
 private:
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
+	
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
 };
