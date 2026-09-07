1e75f1d950bb013062fd16208a0a8a0c4819b326
tomneo2004 <tomneo2004@gmail.com>
2026-09-07 10:15:13 +0800

    2026/9/7
    - DebuffNiagaraComponent now using replication to activate/deactivate particle system on client side

--- Source/Aura/Private/AbilitySystem/Debuff/DebuffNiagaraComponent.cpp
+++ Source/Aura/Private/AbilitySystem/Debuff/DebuffNiagaraComponent.cpp
@@ -2,14 +2,17 @@
 
 
 #include "AbilitySystem/Debuff/DebuffNiagaraComponent.h"
-
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
 #include "Interface/CombatInterface.h"
+#include "Net/UnrealNetwork.h"
 
 UDebuffNiagaraComponent::UDebuffNiagaraComponent()
 {
 	bAutoActivate = false;
+	
+	// Set replication as default
+	SetIsReplicatedByDefault(true);
 }
 
 void UDebuffNiagaraComponent::BeginPlay()
@@ -25,7 +28,9 @@ void UDebuffNiagaraComponent::BeginPlay()
 			EGameplayTagEventType::NewOrRemoved
 		).AddUObject(this, &UDebuffNiagaraComponent::OnDebuffTagChanged);
 	}
-	else if (CombatInterface)
+	
+	// is not necessary to do so as we are using replication from server to client
+	/*else if (CombatInterface)
 	{
 		CombatInterface->GetOnASCRegisteredDelegate().AddWeakLambda(this, 
 			[this](UAbilitySystemComponent* InASC)
@@ -36,7 +41,8 @@ void UDebuffNiagaraComponent::BeginPlay()
 				).AddUObject(this, &UDebuffNiagaraComponent::OnDebuffTagChanged);
 			}
 		);
-	}
+	}*/
+	
 	if (CombatInterface)
 	{
 		CombatInterface->GetOnDeathDelegate().AddDynamic(
@@ -46,20 +52,59 @@ void UDebuffNiagaraComponent::BeginPlay()
 	}
 }
 
+void UDebuffNiagaraComponent::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(UDebuffNiagaraComponent, bIsActivated);
+}
+
 void UDebuffNiagaraComponent::OnDebuffTagChanged(const FGameplayTag InDebuffTag, int32 NewCount)
 {
 	const bool bIsValidOwner = IsValid(GetOwner());
 	const bool bIsAlive = GetOwner()->Implements<UCombatInterface>() && !ICombatInterface::Execute_IsDead(GetOwner());
 	if (NewCount > 0 && bIsValidOwner && bIsAlive)
 	{
-		Activate();
+		ActivateDebuffEffect();
 	}else
 	{
-		Deactivate();
+		DeactivateDebuffEffect();
 	}
 }
 
 void UDebuffNiagaraComponent::OnOwnerDeath(AActor* DeadActor)
 {
-	Deactivate();
+	DeactivateDebuffEffect();
+}
+
+void UDebuffNiagaraComponent::OnRep_IsActivated()
+{
+	if (bIsActivated)
+	{
+		Activate();
+	}
+	else
+	{
+		Deactivate();
+	}
+}
+
+void UDebuffNiagaraComponent::ActivateDebuffEffect()
+{
+	// Only run on server side
+	if (GetOwner()->HasAuthority())
+	{
+		bIsActivated = true;
+		Activate();
+	}
+}
+
+void UDebuffNiagaraComponent::DeactivateDebuffEffect()
+{
+	// Only run on server side
+	if (GetOwner()->HasAuthority())
+	{
+		bIsActivated = false;
+		Deactivate();
+	}
 }
--- Source/Aura/Public/AbilitySystem/Debuff/DebuffNiagaraComponent.h
+++ Source/Aura/Public/AbilitySystem/Debuff/DebuffNiagaraComponent.h
@@ -21,6 +21,13 @@ public:
 	UPROPERTY(VisibleAnywhere)
 	FGameplayTag DebuffTag;
 	
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+protected:
+	UPROPERTY(ReplicatedUsing=OnRep_IsActivated)
+	bool bIsActivated = false;
+	
 protected: // Override
 	virtual void BeginPlay() override;
 	
@@ -29,4 +36,21 @@ protected:
 	
 	UFUNCTION()
 	void OnOwnerDeath(AActor* DeadActor);
+
+	/**
+	 * Callback function for client when bIsActivated changed on server side
+	 */
+	UFUNCTION()
+	void OnRep_IsActivated();
+	
+private:
+	/**
+	 * Handle Niagara activation with network consideration
+	 */
+	void ActivateDebuffEffect();
+
+	/**
+	 * Handle Niagara deactivation with network consideration
+	 */
+	void DeactivateDebuffEffect();
 };
